2b79e94dee86f6ff6bd4b6c4398f9c5f25e2ee97
Matthew Zegar <matthewzegar@gmail.com>
2023-11-09 22:16:54 -0500

    Setup prose for blog pages

--- src/routes/(app)/blog/[slug]/+page.svelte
+++ src/routes/(app)/blog/[slug]/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import Icon from '@iconify/svelte';
 
 	export let data: PageData;
 
@@ -7,8 +8,17 @@
 	$: component = data.component as unknown as C;
 </script>
 
-<p>{data.frontmatter.title}</p>
-<p>{data.frontmatter.author}</p>
-<p>{data.frontmatter.date}</p>
-
-<svelte:component this={component} />
+<article
+	class="font-poppins prose prose-stone prose-h1:m-0 lg:prose-xl container mx-auto px-4 py-8"
+>
+	<h1>{data.frontmatter.title}</h1>
+	<div class="flex items-center text-xl">
+		<Icon icon="ic:baseline-calendar-today" class="mr-1" />
+		{new Date(data.frontmatter.date).toLocaleString('en-us', {
+			month: 'short',
+			day: 'numeric',
+			year: 'numeric'
+		})}
+	</div>
+	<svelte:component this={component} />
+</article>
