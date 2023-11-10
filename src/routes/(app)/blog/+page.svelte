a649b801d4254f93f73e5fad036c1b3e8515135c
Matthew Zegar <matthewzegar@gmail.com>
2023-11-09 19:26:44 -0500

    Resolve blog slug in a more programmatic way

--- src/routes/(app)/blog/+page.svelte
+++ src/routes/(app)/blog/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+	import { page } from '$app/stores';
 	import Icon from '@iconify/svelte';
 
 	export let data: PageData;
@@ -7,7 +8,7 @@
 
 <div class="flex flex-col gap-10 mx-auto px-4 py-20">
 	{#each data.posts as { slug, title, date }}
-		<a href="/blog/{slug}" class="transition-all hover:scale-110 font-poppins">
+		<a href="{$page.url.pathname}/{slug}" class="transition-all hover:scale-110 font-poppins">
 			<p class="text-4xl font-medium underline">{title}</p>
 			<div class="flex items-center text-xl">
 				<Icon icon="ic:baseline-calendar-today" class="mr-1" />
