c425235d7e5be4c6a228fab7cbee8c08ac679feb
Matthew Zegar <matthewzegar@gmail.com>
2023-11-11 11:29:25 -0500

    Just use the function directly?

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -1,16 +1,12 @@
 import type { EntryGenerator } from './$types';
 
-function slugFromPath(path: string) {
-	return path.match(/([\w-]+)\.(svelte\.md|md|svx)/i)?.[1] ?? null;
-}
-
 export const entries: EntryGenerator = async () => {
 	const modules = import.meta.glob(`/src/blogs/*.{md,svx,svelte.md}`);
 	const postPromises = Object.entries(modules).map(([path, resolver]) =>
 		resolver().then(
 			(post) =>
 				({
-					slug: slugFromPath(path),
+					slug: path.match(/([\w-]+)\.(svelte\.md|md|svx)/i)?.[1] ?? null,
 					...(post as unknown as App.MdsvexFile).metadata
 				}) as App.BlogPost
 		)
