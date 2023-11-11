81157474f99a18ab264e89c0767e21725abe2dd9
Matthew Zegar <matthewzegar@gmail.com>
2023-11-10 22:07:15 -0500

    We should be using a canvas instead

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,18 +1,4 @@
 <script lang="ts">
-	import Draggable from '$lib/components/Draggable.svelte';
 </script>
 
-<div class="bg-[url('/team/pxgrid.png')] h-screen">
-	<Draggable>
-		<div class="map" />
-	</Draggable>
-</div>
-
-<style>
-	.map {
-		image-rendering: pixelated;
-		background-image: url('/team/pxmap.png');
-		height: 500px;
-		width: 500px;
-	}
-</style>
+<div>team page</div>
