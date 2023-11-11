314ed70cf6467f525e720f1f0d1a70fe74b22f41
Matthew Zegar <matthewzegar@gmail.com>
2023-11-11 11:40:46 -0500

    Add some more static options to svelte static config

--- svelte.config.js
+++ svelte.config.js
@@ -13,15 +13,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true,
+			fallback: '404.html'
+		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		},
-		prerender: {
-			handleHttpError: ({ path, referrer, message }) => {
-				throw new Error(message);
-			},
-			entries: []
 		}
 	},
 	extensions: ['.svelte', '.md'],
