2ee9b33a8767587da529c5866e4646ba39e44970
Matthew Zegar <matthewzegar@gmail.com>
2023-11-16 18:03:02 -0500

    Hyperlink any names in the heading component

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+	import { Members } from '$lib/types/Member';
+
 	export let h: string = 'h2';
 	export let title: string;
 	export let caption: string;
+
+	function embedHyperlinks(text: string) {
+		for (let i = 0; i < Members.length; ++i) {
+			const member = Members[i];
+			const regex = new RegExp(member.name, 'gi');
+			text = text.replace(regex, `<a href="/team#${member.name}">${member.name}</a>`);
+		}
+
+		return text;
+	}
 </script>
 
 <div>
@@ -9,7 +21,7 @@
 		<svelte:element this={h} id={title} class:caption>{title}</svelte:element></a
 	>
 	{#if caption}
-		<p>{caption}</p>
+		<p>{@html embedHyperlinks(caption)}</p>
 	{/if}
 </div>
 
