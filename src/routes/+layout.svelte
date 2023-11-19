4d88483712b30d592fc7a94e52ac31aadef03b89
Matthew Zegar <matthewzegar@gmail.com>
2023-11-18 20:26:14 -0500

    Update bottom hamburger message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -100,7 +100,7 @@
 			</div>
 			<div class="grow" />
 			<div class="text-center font-poppins text-white">
-				<p class="text-2xl font-medium">small fish</p>
+				<p class="text-2xl font-medium">made with ❤️</p>
 				<p>even inconvenient rocks can't stop us</p>
 				<img src="home/rock.gif" alt="rock" class="px-28" />
 			</div>
