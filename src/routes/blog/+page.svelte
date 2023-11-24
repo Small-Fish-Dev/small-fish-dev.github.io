96550f46d5d8af178cf16d78be9e7cfd552112a5
Matthew Zegar <matthewzegar@gmail.com>
2023-11-23 23:08:04 -0500

    Show month year for blogs page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -9,6 +9,24 @@
 		if (!event.target) return;
 		event.target.src = '/team/profiles/none.jpg';
 	};
+
+	function groupPostsByMonthYear(posts: any[]) {
+		const groupedPosts: Record<string, any> = {};
+
+		posts.forEach((post) => {
+			const key = new Date(post.date).toLocaleString('default', { month: 'long', year: 'numeric' });
+
+			if (!groupedPosts[key]) {
+				groupedPosts[key] = [];
+			}
+
+			groupedPosts[key].push(post);
+		});
+
+		return groupedPosts;
+	}
+
+	const groupedPosts = groupPostsByMonthYear(data.posts);
 </script>
 
 <div class="fixed h-full w-full animate-scroll bg-pixel-white bg-pixel-large" />
@@ -16,60 +34,65 @@
 <div
 	class="md:px-18 container z-10 mx-auto mb-20 flex flex-col gap-8 px-2 pt-32 font-poppins sm:px-4 lg:px-32 xl:px-40"
 >
-	{#each data.posts as post}
-		<a
-			href="{$page.url.pathname}/{post.slug}"
-			class="shadow-md transition-all text-shadow hover:scale-105"
-		>
-			<div class="flex flex-col overflow-hidden md:flex-row">
-				<div class="relative h-48 w-full md:h-auto md:basis-1/2">
-					{#if post.thumbnail}
-						<img
-							class="absolute h-full w-full border-b-2 border-darkblue object-cover md:border-b-0 md:border-r-4"
-							src={`/blogs/${post.slug}/${post.thumbnail}`}
-							alt="thumbnail"
-						/>
-					{/if}
-				</div>
-				<div class="gradient flex w-full flex-col justify-center break-words p-4 md:p-8">
-					<div class="flex flex-row flex-wrap justify-between gap-4 pb-4">
-						{#if post.member}
-							<div class="flex origin-left flex-row items-center gap-2 text-gray">
-								<img
-									class="h-[42px] w-[42px] bg-cover shadow-xs"
-									src={post.member.avatar == null ? '/team/profiles/none.jpg' : post.member.avatar}
-									alt="publisher"
-									on:error={imageFallback}
+	{#each Object.entries(groupedPosts) as [monthYear, postsInMonthYear]}
+		<h1 class="text-3xl font-bold underline decoration-blue">{monthYear}</h1>
+		{#each postsInMonthYear as post}
+			<a
+				href="{$page.url.pathname}/{post.slug}"
+				class="shadow-md transition-all text-shadow hover:scale-105"
+			>
+				<div class="flex flex-col overflow-hidden md:flex-row">
+					<div class="relative h-48 w-full md:h-auto md:basis-1/2">
+						{#if post.thumbnail}
+							<img
+								class="absolute h-full w-full border-b-2 border-darkblue object-cover md:border-b-0 md:border-r-4"
+								src={`/blogs/${post.slug}/${post.thumbnail}`}
+								alt="thumbnail"
+							/>
+						{/if}
+					</div>
+					<div class="gradient flex w-full flex-col justify-center break-words p-4 md:p-8">
+						<div class="flex flex-row flex-wrap justify-between gap-4 pb-4">
+							{#if post.member}
+								<div class="flex origin-left flex-row items-center gap-2 text-gray">
+									<img
+										class="h-[42px] w-[42px] bg-cover shadow-xs"
+										src={post.member.avatar == null
+											? '/team/profiles/none.jpg'
+											: post.member.avatar}
+										alt="publisher"
+										on:error={imageFallback}
+									/>
+									<p class="font-medium">
+										by <span class="font-bold text-gray transition-all">{post.member.name}</span>
+									</p>
+								</div>
+							{/if}
+							<div class="flex items-center text-lg font-medium text-white">
+								<Icon
+									icon="ic:baseline-calendar-today"
+									class="mr-1"
+									style="filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.5));"
 								/>
-								<p class="font-medium">
-									by <span class="font-bold text-gray transition-all">{post.member.name}</span>
+								<p>
+									{new Date(post.date).toLocaleString('en-us', {
+										month: 'short',
+										day: 'numeric',
+										year: 'numeric'
+									})}
 								</p>
 							</div>
-						{/if}
-						<div class="flex items-center text-lg font-medium text-white">
-							<Icon
-								icon="ic:baseline-calendar-today"
-								class="mr-1"
-								style="filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.5));"
-							/>
-							<p>
-								{new Date(post.date).toLocaleString('en-us', {
-									month: 'short',
-									day: 'numeric',
-									year: 'numeric'
-								})}
-							</p>
 						</div>
+
+						<h2 class="text-4xl font-medium text-white">{post.title}</h2>
+
+						{#if post.description}
+							<p class="text-white">{post.description}</p>
+						{/if}
 					</div>
-
-					<h2 class="text-4xl font-medium text-white">{post.title}</h2>
-
-					{#if post.description}
-						<p class="text-white">{post.description}</p>
-					{/if}
 				</div>
-			</div>
-		</a>
+			</a>
+		{/each}
 	{/each}
 </div>
 
