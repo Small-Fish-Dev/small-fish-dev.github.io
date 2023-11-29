41c5e8da71aa7621577176b8092bf51f1f132c7a
Matthew Zegar <matthewzegar@gmail.com>
2023-11-28 19:05:22 -0500

    Undo previous changes

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -211,7 +211,7 @@
 	}
 </script>
 
-<div class="overflow h-screen w-screen overflow-hidden">
+<div class="h-screen w-full overflow-hidden">
 	<div class="scroll h-full w-full bg-[url('/team/pxgrid.png')]">
 		{#await promise then options}
 			<canvas
@@ -231,7 +231,7 @@
 		<div
 			in:fly={{ duration: 300, x: '100%', opacity: 0.5, easing: quintOut }}
 			out:fly={{ duration: 300, x: '100%', opacity: 0.5 }}
-			class="pointer-events-none absolute right-0 top-0 z-20 flex h-full w-[320px] max-w-[450px] p-4 pb-16 md:w-96 md:p-8"
+			class="pointer-events-none absolute right-0 top-0 z-20 flex h-full w-[320px] max-w-[450px] p-4 md:w-96 md:p-8"
 		>
 			<MemberCard {member} onClose={() => tryOpenCard()} />
 		</div>
