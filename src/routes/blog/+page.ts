8474f4d1427561c7fbde399ec78f199464b455e9
ceitine <ceitinee@gmail.com>
2024-03-20 12:44:46 +0200

    filter out non-published, published default to true

--- src/routes/blog/+page.ts
+++ src/routes/blog/+page.ts
@@ -2,6 +2,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
 	const response = await fetch(`/api/posts`);
-	const posts: App.BlogPost[] = await response.json();
+	const posts: App.BlogPost[] = (await response.json())
+		.filter( (post: App.BlogPost) => post.published ?? true );
 	return { posts };
 };
