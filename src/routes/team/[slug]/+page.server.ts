9953a75e637ebefe90a6fa6a444120bd9b50abd4
ceitine <ceitinee@gmail.com>
2024-06-02 19:09:56 +0300

    use lowercase slug entry, fallback metatags

--- src/routes/team/[slug]/+page.server.ts
+++ src/routes/team/[slug]/+page.server.ts
@@ -3,7 +3,7 @@ import { Members, type Member } from '$lib/types/Member';
 /** @type {import('./$types').EntryGenerator} */
 export function entries() {
 	return Members.map( function(m) {
-        return { slug: decodeURI(m.name) } 
+        return { slug: decodeURI(m.name).toLocaleLowerCase() } 
     } );
 }
 
--- src/routes/team/[slug]/+page.svelte
+++ src/routes/team/[slug]/+page.svelte
@@ -23,5 +23,9 @@
 			<meta property="og:description" content={initialMember.description} />
 		{/if}
 		<meta property="og:image" content="https://smallfi.sh/{initialMember.avatar}" />
+	{:else}
+		<meta property="og:title" content="Small Fish - Team" />
+		<meta property="og:image" content="https://smallfi.sh/common/logo-round.png" />
+		<meta property="og:description" content="Take a look at our talented developers!" />
 	{/if}
 </svelte:head>
