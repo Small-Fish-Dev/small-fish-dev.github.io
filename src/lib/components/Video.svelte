b64972dc57018be2acb4a9741c23463743031499
ceitine <ceitinee@gmail.com>
2024-11-09 21:49:48 +0200

    resolve path on video component too

--- src/lib/components/Video.svelte
+++ src/lib/components/Video.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 
+	function resolvePath(src: string) {
+		return src.startsWith('http://') || src.startsWith('https://')
+			? src
+			: `/blogs/${$page.params.slug}/${src}`;
+	}
+
 	export let src: string;
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-media-has-caption -->
 <video class="mx-auto flex justify-center rounded-lg" controls>
-	<source src="/blogs/{$page.params.slug}/{src}" />
+	<source src={resolvePath(src)} />
 </video>
