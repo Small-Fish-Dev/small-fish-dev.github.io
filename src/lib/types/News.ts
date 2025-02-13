c105a0bb8ffe7f17804284abc2c2085e29979b74
ceitine <ceitinee@gmail.com>
2025-02-13 22:51:08 +0200

    replace underscores with spaces on news package

--- src/lib/types/News.ts
+++ src/lib/types/News.ts
@@ -1,78 +1,76 @@
-const API_ENDPOINT: string = "https://services.facepunch.com/sbox";
-const NEWS_API: string = "news/organization/fish";
+const API_ENDPOINT: string = 'https://services.facepunch.com/sbox';
+const NEWS_API: string = 'news/organization/fish';
 
 interface BackendUser {
-    Id: bigint;
-    Name: string;
-    Url: string;
+	Id: bigint;
+	Name: string;
+	Url: string;
 }
-  
+
 interface NewsSection {
-    Id: string;
-    Title: string;
-    Author: BackendUser;
-    SortOrder: number;
-    Contents: string;
-    Slug: string;
+	Id: string;
+	Title: string;
+	Author: BackendUser;
+	SortOrder: number;
+	Contents: string;
+	Slug: string;
 }
 
 interface NewsPost {
-    Id: string;
-    Url: string;
-    Created: string;
-    Title: string;
-    Summary: string;
-    Author: BackendUser;
-    Media: string;
-    Sections: NewsSection[];
-    Package: string;
+	Id: string;
+	Url: string;
+	Created: string;
+	Title: string;
+	Summary: string;
+	Author: BackendUser;
+	Media: string;
+	Sections: NewsSection[];
+	Package: string;
 }
 
 export type NewsEntry = {
-    url: string;
-    date: Date;
-    title: string;
-    summary: string;
-    thumbnail: string;
-    package: string | undefined;
-    source: string;
-}
+	url: string;
+	date: Date;
+	title: string;
+	summary: string;
+	thumbnail: string;
+	package: string | undefined;
+	source: string;
+};
 
-export const BlogAsNews = function(blog: App.BlogPost): NewsEntry {
-    return {
-        url: `${location.origin}/blog/${blog.slug}`,
-        thumbnail: `/blogs/${blog.slug}/${blog.thumbnail}`,
-        date: new Date(blog.date),
-        title: blog.title,
-        summary: blog.description,
-        source: 'Fishblog'
-    } as NewsEntry;
-}
+export const BlogAsNews = function (blog: App.BlogPost): NewsEntry {
+	return {
+		url: `${location.origin}/blog/${blog.slug}`,
+		thumbnail: `/blogs/${blog.slug}/${blog.thumbnail}`,
+		date: new Date(blog.date),
+		title: blog.title,
+		summary: blog.description,
+		source: 'Fishblog'
+	} as NewsEntry;
+};
 
-export const NewsAsNews = function(news: NewsPost): NewsEntry {
-    return {
-        url: `https://sbox.game${news.Url}`,
-        thumbnail: news.Media,
-        date: new Date(news.Created),
-        title: news.Title,
-        summary: news.Summary,
-        package: news.Package.substring(5),
-        source: 'sbox.game'
-    } as NewsEntry;
-}
+export const NewsAsNews = function (news: NewsPost): NewsEntry {
+	return {
+		url: `https://sbox.game${news.Url}`,
+		thumbnail: news.Media,
+		date: new Date(news.Created),
+		title: news.Title,
+		summary: news.Summary,
+		package: news.Package.substring(5).replaceAll('_', ' '),
+		source: 'sbox.game'
+	} as NewsEntry;
+};
 
 export const FetchNewsAsync = async function (): Promise<NewsPost[] | null> {
-    const response = await fetch(`${API_ENDPOINT}/${NEWS_API}`);
+	const response = await fetch(`${API_ENDPOINT}/${NEWS_API}`);
 
-    // We failed our request...
-    if(!response.ok)
-        return null;
+	// We failed our request...
+	if (!response.ok) return null;
 
-    // Get the body.
-    const body = await response.text();
-    if(body == null)
-        return null;
-    
-    const posts: NewsPost[] = JSON.parse(body);
-    return posts;
-}
+	// Get the body.
+	const body = await response.text();
+	if (body == null) return null;
+
+	const posts: NewsPost[] = JSON.parse(body);
+	return posts;
+};
